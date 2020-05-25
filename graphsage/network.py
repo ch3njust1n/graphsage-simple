2f26fb9f8f7084718a470a025253676587774ba9
ch3njust1n <ch3njus@gmail.com>
2020-05-25 12:13:49 -0400

    Refactored supervisedgraphsage into its own file and deleted pubmed functions

---
+++ graphsage/network.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+"""
+Simple supervised GraphSAGE model as well as examples running the model
+on the Cora and Pubmed datasets.
+"""
+
+class SupervisedGraphSage(nn.Module):
+
+    def __init__(self, num_classes, enc):
+        super(SupervisedGraphSage, self).__init__()
+        self.enc = enc
+        self.xent = nn.CrossEntropyLoss()
+
+        self.weight = nn.Parameter(torch.FloatTensor(num_classes, enc.embed_dim))
+        init.xavier_uniform(self.weight)
+
+    def forward(self, nodes):
+        embeds = self.enc(nodes)
+        scores = self.weight.mm(embeds)
+        return scores.t()
+
+    def loss(self, nodes, labels):
+        scores = self.forward(nodes)
+        return self.xent(scores, labels.squeeze())
